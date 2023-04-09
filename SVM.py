f90a1af104d51dfe190b5876e437e9d181bfe542
Adrian Panek <adrian.panek999@gmail.com>
2023-04-09 11:39:56 +0200

    Initial commit, base class implemented

---
+++ SVM.py
@@ -0,0 +1,17 @@
+import numpy
+from sklearn.datasets import make_classification
+
+class SVM():
+    def __init__(self):
+        pass
+
+    def fit(self, X, y):
+        pass
+
+    def predict(self, X):
+        pass
+
+
+x, y = make_classification(n_samples=500)
+
+print(x)
