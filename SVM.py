801c89cf183107b63badfc4da6f0d941ff8e529f
Adrian Panek <adrian.panek999@gmail.com>
2023-04-11 21:08:31 +0200

    added scikit-learn implemented algorithm to compare

--- SVM.py
+++ SVM.py
@@ -1,11 +1,10 @@
-import numpy
+import numpy as np
+import ipdb
 from pandas import read_csv
-from sklearn.datasets import make_classification
+from sklearn.datasets import make_blobs, make_classification, make_circles
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 from sklearn.base import BaseEstimator
-
-#learning_rate = 0.001
-#lambda = 0.01
-
 
 class SVM(BaseEstimator):
     def __init__(self):
@@ -18,39 +17,63 @@ class SVM(BaseEstimator):
         y_pos_neg = [-1 if x<=0 else 1 for x in y]
         
         #inicjalizacja wektorów
-        self.w = numpy.zeros(2)
+        self.w = np.zeros(X.shape[1])
         self.b = 0
 
-        i=0
-        for val in X:
-            #przypadek, gdy badana próbka znajduje się w dobrej klasie
-            if (y_pos_neg[i] * (numpy.dot(X[i], self.w) - self.b ) >= 1 ):
-                #aktualizacja w
-                self.w = self.w-self.learning_rate * (2 * self.la * self.w)
-                #w tym przypadku b pozostaje niezmienione
-            else:
-                self.w = self.w - self.learning_rate * ((2 * self.la * self.w) - numpy.dot(y_pos_neg[i], X[i]))
-                self.b = self.b - (self.learning_rate * y_pos_neg[i])
-            i+=1
+        for _ in range(50):
+            i=0
+            for val in X:
+                #przypadek, gdy badana próbka znajduje się w dobrej klasie
+                if (y_pos_neg[i] * (np.dot(X[i], self.w) - self.b ) >= 1 ):
+                    #aktualizacja w
+                    self.w = self.w-self.learning_rate * (2 * self.la * self.w)
+                    #w tym przypadku b pozostaje niezmienione
+                #przypadek, gdy próbka znajduje się w złej klasie
+                else:
+                    self.w = self.w - self.learning_rate * ((2 * self.la * self.w) - np.dot(y_pos_neg[i], X[i]))
+                    self.b = self.b - (self.learning_rate * y_pos_neg[i])
+                i+=1
 
     def predict(self, X):
-        y = numpy.dot(X, self.w) - self.b
-        return 1 if y>=0 else -1
+        y_pred = []
+        for x_i in X:
+            y_i = np.dot(x_i, self.w) - self.b
+            y_i = 1 if y_i >=0 else 0
+            y_pred.append(y_i)
+        return y_pred
+     
 
-dataset = read_csv("dataset/Cancer_Data.csv")
-y = []
-x = [[rad_mn, txt_mn] for rad_mn, txt_mn in zip(dataset.radius_mean, dataset.texture_mean)]
+def RBF(X, gamma):
+    ipdb.set_trace()
+    print(X[:, 0]) # pierwszy wektor
+    print(X[:, 1]) # drugi wektor
 
-for val in dataset.diagnosis:
-    if val == 'M':
-        y.append(1)
-    y.append(0)
 
-cls = SVM()
-cls.fit(x,y)
-print(cls.predict(x[1]))
-print(y[1])
+X, y = make_circles(n_samples=10, noise=0.06, random_state=42)
 
-# print(f"{cls.b}, {cls.w}")
-# import ipdb
-# ipdb.set_trace()
+# X = RBF(X, gamma=None)
+
+
+# testy zaimplementowanego klasyfikatora
+MyClassifier = SVM()
+MyClassifier.fit(X,y)
+MyClass_pred = MyClassifier.predict(X)
+print(f"My implemented Classifier prediction: {MyClass_pred}")
+# print(f"My implemented Classifier accuracy: {accuracy_score(y, MyClass_pred)}")
+
+ImplementedClassifier = SVC()
+ImplementedClassifier.fit(X,y)
+ImplementedClass_pred = ImplementedClassifier.predict(X)
+print(f"Scikit-learn implemented Classifier prediction: {ImplementedClass_pred}")
+# print(f"Scikit-learn implemented Classifier accuracy: {accuracy_score(y, ImplementedClass_pred)}")
+
+
+
+# dataset = read_csv("dataset/Cancer_Data.csv")
+# y = []
+# X = [[rad_mn, txt_mn] for rad_mn, txt_mn in zip(dataset.radius_mean, dataset.texture_mean)]
+
+# for val in dataset.diagnosis:
+#     if val == 'M':
+#         y.append(1)
+#     y.append(0)
