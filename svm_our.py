5e8a5054cc12c5593ce3fe49a545ba4667f6364d
adrian-panek <adrian.panek999@gmail.com>
2023-05-10 15:44:33 +0200

    added kernel to our implementation of svm

--- svm_our.py
+++ svm_our.py
@@ -1,12 +1,13 @@
 import numpy as np
 
-from SVM import SVM
+from SVM import SVM, RBF
 
 from sklearn.datasets import make_classification
 from sklearn.metrics import f1_score, precision_score, accuracy_score
 from sklearn.model_selection import cross_val_score
 
 X, y = make_classification(n_samples=100, n_features=2, n_informative=2, n_redundant=0, n_repeated=0, random_state=59)
+X = RBF(X, 0.5)
 
 MyClassifier = SVM(0.001, 0.01, 100)
 MyClassifier.fit(X,y)
