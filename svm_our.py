0b3031f454d1c5720af10018d90b135dd4613dec
adrian-panek <adrian.panek999@gmail.com>
2023-05-10 16:09:32 +0200

    implemented data split in each dataset

--- svm_our.py
+++ svm_our.py
@@ -4,16 +4,17 @@ from SVM import SVM, RBF
 
 from sklearn.datasets import make_classification
 from sklearn.metrics import f1_score, precision_score, accuracy_score
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score, train_test_split
 
-X, y = make_classification(n_samples=100, n_features=2, n_informative=2, n_redundant=0, n_repeated=0, random_state=59)
+X, y = make_classification(n_samples=1000, n_features=2, n_informative=2, n_redundant=0, n_repeated=0, random_state=59)
 X = RBF(X, 0.5)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, train_size=0.8)
 
 MyClassifier = SVM(0.001, 0.01, 100)
-MyClassifier.fit(X,y)
-MyClass_pred = MyClassifier.predict(X)
+MyClassifier.fit(X_train, y_train)
+MyClass_pred = MyClassifier.predict(X_test)
 
-print(f"F1 score score for My Implemented Classifier is: {f1_score(y, MyClass_pred, average='macro')}")
-print(f"Precision score for My Implemented Classifier is: {precision_score(y, MyClass_pred, average='macro')}")
-print(f"Accuracy score for My Implemented Classifier is: {accuracy_score(y, MyClass_pred)}")
+print(f"F1 score score for My Implemented Classifier is: {f1_score(y_test, MyClass_pred, average='macro')}")
+print(f"Precision score for My Implemented Classifier is: {precision_score(y_test, MyClass_pred, average='macro')}")
+print(f"Accuracy score for My Implemented Classifier is: {accuracy_score(y_test, MyClass_pred)}")
 print(f"Mean of cross val score for Scikit-learn Implemented Classifier is: {np.mean(cross_val_score(MyClassifier, X, y, cv=5, scoring='accuracy'))}")
--- svm_sklearn.py
+++ svm_sklearn.py
@@ -4,16 +4,18 @@ from sklearn.svm import SVC
 
 from sklearn.datasets import make_classification
 from sklearn.metrics import f1_score, precision_score, accuracy_score
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score, train_test_split
 
-X, y = make_classification(n_samples=100, n_features=2, n_informative=2, n_redundant=0, n_repeated=0, random_state=59)
+X, y = make_classification(n_samples=1000, n_features=2, n_informative=2, n_redundant=0, n_repeated=0, random_state=59)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, train_size=0.8)
+
 
 ImplementedClassifier = SVC()
-ImplementedClassifier.fit(X,y)
-ImplementedClass_pred = ImplementedClassifier.predict(X)
+ImplementedClassifier.fit(X_train, y_train)
+ImplementedClass_pred = ImplementedClassifier.predict(X_test)
 
-print(f"F1 score score for Scikit-learn Implemented Classifier is: {f1_score(y, ImplementedClass_pred, average='macro')}")
-print(f"Precision score for Scikit-learn Implemented Classifier is: {precision_score(y, ImplementedClass_pred, average='macro')}")
-print(f"Accuracy score for Scikit-learn Implemented Classifier is: {accuracy_score(y, ImplementedClass_pred)}")
+print(f"F1 score score for Scikit-learn Implemented Classifier is: {f1_score(y_test, ImplementedClass_pred, average='macro')}")
+print(f"Precision score for Scikit-learn Implemented Classifier is: {precision_score(y_test, ImplementedClass_pred, average='macro')}")
+print(f"Accuracy score for Scikit-learn Implemented Classifier is: {accuracy_score(y_test, ImplementedClass_pred)}")
 print(f"Mean of cross val score for Scikit-learn Implemented Classifier is: {np.mean(cross_val_score(ImplementedClassifier, X, y, cv=5))}")
 
